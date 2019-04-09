22f4dbef184ee366fe2bc63cd26b45cf5beee7b0
Bosenator <strooptroopers+ghbose@gmail.com>
2019-04-09 17:19:06 +0100

    Add more XEH, begin functions structuring

--- addons/bloodsplatter/CfgEventHandlers.hpp
+++ addons/bloodsplatter/CfgEventHandlers.hpp
@@ -3,3 +3,25 @@ class Extended_PreInit_EventHandlers {
 		init = "call compile preprocessFileLineNumbers 'XEH_preInit.sqf';";
 	};
 };
+
+class Extended_InitPost_EventHandlers {
+	class CAManBase {
+		class BloodSplatter_Init {
+			init = "[_this select 0] call BloodLust_fnc_InitUnit;";
+		};
+	};
+
+	class AllVehicles {
+		class BloodSplatter_Init {
+			init = "[_this select 0] call BloodLust_fnc_InitVehicle;";
+		};
+	};
+};
+
+class Extended_Respawn_EventHandlers {
+	class CAManBase {
+		class BloodSplatter_Respawn {
+			respawn = "_this call BloodLust_fnc_handleRespawn;";
+		};
+	};
+};
---
+++ addons/bloodsplatter/CfgFunctions.hpp
@@ -0,0 +1,12 @@
+class CfgFunctions {
+	class BloodSplatter {
+		class functions {
+			file = "bloodsplatter\functions";
+			
+			/** INIT AND HANDLERS **/
+			class initUnit {};
+			class initVehicle {};
+			class handleRespawn {};
+		};
+	};
+};
